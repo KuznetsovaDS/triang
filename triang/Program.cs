deaa7a1b3e15c4a293c42df96c39cec516272a54
Darya Kuznetsova <Dashar1996@gmail.com>
2023-11-18 14:03:17 +0300

    Добавьте файлы проекта.

---
+++ triang/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите периметр");
+        double perim = Convert.ToDouble(Console.ReadLine());
+        double side = perim / 3;
+        double square = Math.Sqrt(3 * Math.Pow(side, 4) / 16);
+        Console.WriteLine("Сторона                   " + "Площадь" + "\n" + side + "\t" + square);
+
+    }
+}
